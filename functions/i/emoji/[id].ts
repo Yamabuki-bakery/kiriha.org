3d80af50433826e6e528f104d073488db69b5ec6
codehz <git@hertz.moe>
2024-03-02 18:09:02 +0800

    remove cache

--- functions/i/emoji/[id].ts
+++ functions/i/emoji/[id].ts
@@ -6,10 +6,5 @@ export const onRequest: PagesFunction<Env, "id"> = async ({
   const httpres = await fetch(`https://t.me/i/emoji/${id}.json`);
   if (!httpres.ok) return new Response(null, { status: 404 });
   const { emoji } = (await httpres.json()) as { emoji: string };
-  let response = await fetch(emoji, { cf: { cacheTtl: 259_200 } });
-  if (response.ok) {
-    response = response.clone();
-    response.headers.set("Cache-Control", "max-age=259200");
-  }
-  return response;
+  return await fetch(emoji, { cf: { cacheTtl: 259_200 } });
 };
